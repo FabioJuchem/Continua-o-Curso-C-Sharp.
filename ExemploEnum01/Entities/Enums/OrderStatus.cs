e5dc5d3c09232ccecce646381ccd66b75dd623c3
FabioJuchem <fabiojuchem@gmail.com>
2019-01-30 13:14:25 -0200

    Adicionado mais alguns exercicios

---
+++ ExemploEnum01/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿ 
+
+namespace ExemploEnum01.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+         PendingPayment = 0,
+         Processing = 1,
+         Shipped = 2,
+         Delivered = 3
+    }
+}
---
+++ ExemploEnum01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ExemploEnum01.Entities;
+using ExemploEnum01.Entities.Enums;
+
+namespace ExemploEnum01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
